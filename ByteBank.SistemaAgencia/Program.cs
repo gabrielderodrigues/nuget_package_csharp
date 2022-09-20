d69bc273eb153e133438e81ebe5aa59223554460
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-09-20 06:41:15 -0300

    Add project files.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Console.WriteLine($"Agência: {conta.Agencia} | Número: {conta.Numero}");
+
+            Console.ReadLine();
+        }
+    }
+}
